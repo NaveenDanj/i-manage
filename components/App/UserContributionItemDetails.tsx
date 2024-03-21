e5d31441861b9ce463d3ba3732fb947e3e5887ac
naveendanj <naveenhettiwaththa@gmail.com>
2024-03-22 04:38:10 +0530

    added required multiple components

---
+++ components/App/UserContributionItemDetails.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const UserContributionItemDetails = () => {
+  return (
+    <View style={{borderRadius: 20, padding: 10}}>
+      <Text>UserContributionItemDetails</Text>
+    </View>
+  );
+};
+
+export default UserContributionItemDetails;

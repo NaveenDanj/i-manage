340e65f31e4ad0a469da085d1ff9df0e88f710f6
naveendanj <naveenhettiwaththa@gmail.com>
2024-03-22 12:01:09 +0530

    added user contribution components and latest changes

--- components/App/UserContribution.tsx
+++ components/App/UserContribution.tsx
@@ -7,9 +7,22 @@ import Colors from '../Colors';
 const UserContribution = () => {
   return (
     <View style={{display: 'flex', gap: 20, paddingTop: 30}}>
-      <Text style={{fontSize: 16, fontWeight: '700', color: Colors.dark.text}}>
-        2024-03 #3
-      </Text>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{fontSize: 16, fontWeight: '700', color: Colors.dark.text}}>
+          2024-03 #3
+        </Text>
+
+        <Text style={{fontSize: 13, fontWeight: '700', color: '#226AFA'}}>
+          View
+        </Text>
+      </View>
 
       <UserContributionItem />
       <UserContributionItem />
--- components/App/UserContributionItemDetails.tsx
+++ components/App/UserContributionItemDetails.tsx
@@ -1,10 +1,75 @@
-import {StyleSheet, Text, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import {Text, View} from 'react-native';
 import React from 'react';
+import Colors from '../Colors';
 
 const UserContributionItemDetails = () => {
   return (
-    <View style={{borderRadius: 20, padding: 10}}>
-      <Text>UserContributionItemDetails</Text>
+    <View
+      style={{
+        marginTop: 20,
+        borderRadius: 10,
+        padding: 20,
+        backgroundColor: '#181920',
+        gap: 5,
+      }}>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{color: '#D2D2D4'}}>Paid Amount</Text>
+        <Text style={{color: Colors.dark.text}}>LKR 1200.00</Text>
+      </View>
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{color: '#D2D2D4'}}>Installment Amount</Text>
+        <Text style={{color: Colors.dark.text}}>LKR 1200.00</Text>
+      </View>
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{color: '#D2D2D4'}}>Previous Debt</Text>
+        <Text style={{color: Colors.dark.text}}>LKR 650.00</Text>
+      </View>
+
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text
+          style={{
+            color: '#D2D2D4',
+            paddingHorizontal: 5,
+            paddingVertical: 2,
+            backgroundColor: 'red',
+            borderRadius: 5,
+          }}>
+          New Debt
+        </Text>
+        <Text
+          style={{
+            color: '#D2D2D4',
+            paddingHorizontal: 5,
+            paddingVertical: 2,
+            backgroundColor: 'red',
+            borderRadius: 5,
+          }}>
+          LKR 650.00
+        </Text>
+      </View>
     </View>
   );
 };
